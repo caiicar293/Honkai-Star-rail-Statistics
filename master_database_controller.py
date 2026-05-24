12560e82b68bb2a0bf83ea2b58491a3748f2a497
Timothy Manuel <timothymanuel295@gmail.com>
2026-05-24 16:11:02 -0400

    refactor: new controller

--- master_database_controller.py
+++ master_database_controller.py
@@ -1,9 +1,10 @@
-from database_main import HonkaiDataPlatform
+from database_batch import HonkaiDataPlatform
 from database_archetypes_summary import HonkaiMetaAnalyzer
 from database_distributions_summary import StarRailStatsProcessor
 from database_teams_summary import HonkaiTeamMetaAnalyzer
 from database_duos_summary import HonkaiDuosSummaryAnalyzer
-
+from Appearance_rate_builds import HonkaiStatistics_builds
+from database_character_summary import CharacterMetaAnalyzer
 
 platform = HonkaiDataPlatform()
 platform.orchestrate_update()
@@ -18,22 +19,23 @@ analyzer1.run_analysis()
 analyzer2 = HonkaiDuosSummaryAnalyzer()
 analyzer2.run_analysis()
 
-
+analyzer3 = CharacterMetaAnalyzer()
+analyzer3.run_analysis()
 
 processor = StarRailStatsProcessor()
 
 try:
     # 1. Standard Modes
-        processor.process_mode("moc_stats_distributions", "Cycles", "node", "DESC", "moc_stats_distributions_summaries")
-        processor.process_mode("pure_fiction_stats_distributions", "Points", "node", "ASC", "pure_fiction_stats_distributions_summaries")
+        processor.process_mode("moc_stats_distributions", "Scores", "node", "DESC", "moc_stats_distributions_summaries")
+        processor.process_mode("pure_fiction_stats_distributions", "Scores", "node", "ASC", "pure_fiction_stats_distributions_summaries")
         processor.process_mode("apoc_stats_distributions", "Scores", "node", "ASC", "apoc_stats_distributions_summaries")
-        processor.process_mode("anomaly_stats_distributions", "Cycles", "floor", "DESC", "anomaly_stats_distributions_summaries")
+        processor.process_mode("anomaly_stats_distributions", "Scores", "floor", "DESC", "anomaly_stats_distributions_summaries")
 
         # 2. Multi-Modes
-        processor.process_multi_mode("moc_stats_dual_distributions", "Cycles", "DESC", "moc_stats_dual_distributions_summaries")
-        processor.process_multi_mode("pure_fiction_stats_dual_distributions", "Points", "ASC", "pure_fiction_stats_dual_distributions_summaries")
+        processor.process_multi_mode("moc_stats_dual_distributions", "Scores", "DESC", "moc_stats_dual_distributions_summaries")
+        processor.process_multi_mode("pure_fiction_stats_dual_distributions", "Scores", "ASC", "pure_fiction_stats_dual_distributions_summaries")
         processor.process_multi_mode("apoc_stats_dual_distributions", "Scores", "ASC", "apoc_dual_stats_distributions_summaries")
-        processor.process_multi_mode("anomaly_stats_triple_distributions", "Cycles", "DESC", "anomaly_triple_stats_distributions_summaries")
+        processor.process_multi_mode("anomaly_stats_triple_distributions", "Scores", "DESC", "anomaly_triple_stats_distributions_summaries")
 
 finally:
     processor.close()
